90b9793b4828e4b40e9b7190d5728c4f12390fd9
Stefan The Coconut <57968327+TheCoconut420@users.noreply.github.com>
2022-10-20 17:49:37 +0200

    fr

--- get_hex.py
+++
@@ -1,5 +0,0 @@
-with open(r"C:\Users\Stefan\Desktop\3_Hygiene_Belehrungsunterlagen-und-Erklaerung_08.pdf", "rb") as file:
-    print("\n")
-    f = file.read(308)
-    l = f.splitlines()
-    print(f.hex(" "))
